009b188f7b23307f1cccf597880b416661793e70
Martin Sferco <martinsferco.ms@gmail.com>
2024-06-21 19:17:38 -0300

    ListSeq detailes

--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -3,7 +3,6 @@ module ListSeq where
 
 import Seq
 import Par
-import Data.List.NonEmpty (append)
 
 
 instance Seq [] where
@@ -26,24 +25,23 @@ instance Seq [] where
   fromList   = fromListList 
 
 
--- * lista
 emptyList :: [a]
 emptyList = []
 
--- * lista
+
 singletonList :: a -> [a]
 singletonList x = [x]
 
--- * lista
+
 lengthList :: [a] -> Int
 lengthList = length
 
--- * lista
+
 nthList :: [a] -> Int -> a
 nthList (x:xs) 0 = x
 nthList (x:xs) n = nthList xs (n-1)
 
--- * lista
+
 tabulateList :: (Int -> a) -> Int -> [a] 
 tabulateList f k = tabulateListAux f k 0
 
@@ -54,7 +52,7 @@ tabulateList f k = tabulateListAux f k 0
                             in 
                               fi : ft
 
--- * lista
+
 mapList :: (a -> b) -> [a] -> [b]
 mapList f [] = []
 mapList f (x:xs) = let
@@ -62,7 +60,7 @@ mapList f (x:xs) = let
                    in
                        fx : fxs
 
--- * lista
+
 filterList :: (a -> Bool) -> [a] -> [a]
 filterList p [] = []
 filterList p (x:xs) = let
@@ -70,19 +68,19 @@ filterList p (x:xs) = let
                       in
                          if px then x : pxs else pxs
 
--- * lista
+
 appendList :: [a] -> [a] -> [a]
 appendList = (++)
 
--- * lista
+
 takeList :: [a] -> Int -> [a]
 takeList xs k = take k xs
 
--- * lista
+
 dropList :: [a] -> Int -> [a]
 dropList xs k = drop k xs
 
--- * lista
+
 showtList :: [a] -> TreeView a ([a])
 showtList []  = EMPTY
 showtList [x] = ELT x
@@ -92,21 +90,23 @@ showtList xs  = let
                 in
                     NODE tl dl
 
--- * lista
+
 showlList :: [a] -> ListView a ([a])
 showlList [] = NIL
 showlList (x:xs) = CONS x xs
 
--- * lista
+
 joinList :: [[a]] -> [a]
 joinList [] = []
 joinList [xs] = xs
 joinList (xs:xss) = appendList xs (joinList xss)
 
+
 reduceList :: (a -> a -> a) -> a -> [a] -> a
 reduceList op b [] = b
 reduceList op b l  = let v = red op l
                      in  op b v
+
 
 red :: (a -> a -> a) -> [a] -> a
 red op [x] = x
@@ -122,7 +122,6 @@ contractListLen _  l@[x]        =  (l,  1)
 contractListLen _  []           =  ([], 0)
 
 
--- * lista
 scanList :: (a -> a -> a) -> a -> [a] -> ([a], a)
 scanList op b []   =  ([], b)
 scanList op b [x]  =  ([b], op b x)
@@ -132,6 +131,7 @@ scanList op b l    =  let
                       in
                         -- Separamos al resultado en dos partes acorde a la especificacion de scan
                         separateScan l'
+
 
 scanWithoutSeparation :: (a -> a -> a) -> a -> [a] -> [a]
 scanWithoutSeparation op b []  =  [b]
@@ -143,7 +143,6 @@ scanWithoutSeparation op b l   =  let
                                     expandList op l ls' 0 (len + 1)
 
 
-
 expandList :: (a -> a -> a) -> [a] -> [a] -> Int -> Int -> [a]
 expandList _  _  _       _ 0 = []
 expandList op s (x':xs') k m =
@@ -153,31 +152,21 @@ expandList op s (x':xs') k m =
                                                   expandList op (advanceTwo s) xs' (k + 1) (m - 1)
               in  operatedElements : recExpand
 
+
 separateScan :: [a] -> ([a], a)
 separateScan [x]      = ([], x)
 separateScan (x : xs) = let (ls, v) = separateScan xs
                         in  (x : ls, v)
 
+
 advanceTwo :: [a] -> [a]
 advanceTwo (x : y : zs) = zs
 advanceTwo _            = []
 
+
 getFirst :: [a] -> a
 getFirst (x : xs) = x
 
--- * lista
+
 fromListList :: [a] -> [a]
 fromListList = id
-
-
-
--- Ejemplos - BORRARLOS ------------------------------------------------
-concatStrings :: String -> String -> String
-concatStrings a b = "(" ++ a ++ " + " ++ b ++ ")"
-
-ejemploSeq :: [String]
-ejemploSeq = fromListList ["x0", "x1", "x2", "x3", "x4", "x5"]
-
-ejemploSeq' :: [String]
-ejemploSeq' = fromListList ["x0", "x1", "x2", "x3", "x4", "x5", "x6"]
--- ---------------------------------------------------------------------
